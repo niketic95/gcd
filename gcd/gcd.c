ce339713355f02e0b878d23585a694a2bd38574c
Stefan Nicetin <niketic95@gmail.com>
2017-10-24 18:20:29 +0200

    Update gcd.c

--- gcd/gcd.c
+++ gcd/gcd.c
@@ -1,70 +1,63 @@
 /** @file gcd.c
- *  @brief Implementacija najmanjeg zajednickog delioca
+ *  @brief Implementation of the Greatest common divisor
  *
- *  Implementacija potrebnih oepratora i implementacija najveceg zajednickog delioca
- * 	pomocu euklidovog algoritma.
+ *  Implementation of the needed operator and the implementation of the greatest common divisor
+ *  using the euclids algorithm
  *
  *  @author Stefan M. Nicetin
  *  @bug No known bugs
  */
 
 #include "gcd.h"
-#include <stdlib.h>
 
-uint_least32_t modop( uint_least32_t a, uint_least32_t b )
+uint_least32_t modop( uint_least32_t a, uint_least32_t b)
 {
-	uint_least32_t temp		   = 1;
+	uint_least32_t temp	   = 1u;
 	uint_least64_t denominator = b;
 
-	/* Eliminisanje deljenja sa nulom */
-	if( denominator == 0 )
+	/* Divide by zero will return 0*/
+	if(denominator == 0u)
 	{
-		exit( EXIT_FAILURE );
-	}
-
-	/* Moguce ubrzanje za velike brojeve */
-	if( a == denominator )
-	{
-		return 0;
-	}
-
-	/* Pomeranje imenioca na krajnje levu stranu brojioca gde temp prati poziciju cifre*/
-	while( denominator <= a )
-	{
-		denominator <<= 1;
-		temp <<= 1;
-	}
-
-	/* Dok se ne dodje do zadnje cifre*/
-	while( temp > 1 )
-	{
-		denominator >>= 1;
-		temp >>= 1;
-
-		if( a >= denominator )
+		a = 0u;
+	}else{
+		/* Shifting the denominator to the left while temporary tracks the digit position */
+		while( denominator <= a )
 		{
-			a -= denominator;
+			denominator <<= 1;
+			temp <<= 1;
+		}
+		/* Until all digits are finished */
+		while( temp > 1u )
+		{
+			denominator >>= 1u;
+			temp >>= 1u;
+
+			if( a >= denominator )
+			{
+				a += (~denominator)+1u;
+			}
 		}
 	}
-
 	return a;
 }
 
-uint_least32_t gcd( uint_least32_t a, uint_least32_t b )
+
+uint_least32_t gcd( uint_least32_t a, uint_least32_t b)
 {
 	uint_least32_t r;
 
-	if( a == 0
-			|| b == 0 )
+	if( (a == 0u)
+			||(b == 0u) )
 	{
-		return a + b;
-	}
-	r = modop( a, b );
-	while( r != 0 )
-	{
-		a = b;
-		b = r;
-		r = modop( a, b );
+		b+=a;
+	}else{
+		r = modop(a,b);
+		while( r != 0u )
+		{
+			a = b;
+			b = r;
+			r = modop(a,b);
+		}
 	}
 	return b;
 }
@@ -73,9 +66,10 @@ uint_least32_t calculate_gcd( const uint_least32_t arr[], const int_least16_t n 
 {
 	uint_least32_t res = arr[ 0 ];
 	int_least16_t  i;
+
 	for( i = 1; i < n; i++ )
 	{
-		res = gcd( arr[ i ], res );
+		res = gcd( arr[ i ], res);
 	}
 	return res;
 }
