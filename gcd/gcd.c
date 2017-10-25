9fbea0dbfccc9a66d99661067c663a8388a4cb31
Stefan Nicetin <niketic95@gmail.com>
2017-10-25 02:07:36 +0200

    Update gcd.c

--- gcd/gcd.c
+++ gcd/gcd.c
@@ -12,9 +12,8 @@
 
 uint_least32_t modop( uint_least32_t a, uint_least32_t b)
 {
-	uint_least32_t temp	   = 1u;
+	uint_least64_t digit_pos = 1u;
 	uint_least64_t denominator = b;
-
 	/* Divide by zero will return 0*/
 	if(denominator == 0u)
 	{
@@ -24,17 +23,17 @@ uint_least32_t modop( uint_least32_t a, uint_least32_t b)
 		while( denominator <= a )
 		{
 			denominator <<= 1;
-			temp <<= 1;
+			digit_pos <<= 1;
 		}
 		/* Until all digits are finished */
-		while( temp > 1u )
+		while( digit_pos > 1u )
 		{
 			denominator >>= 1u;
-			temp >>= 1u;
+			digit_pos >>= 1u;
 
 			if( a >= denominator )
 			{
-				a += (~denominator)+1u;
+				a -= denominator;
 			}
 		}
 	}
@@ -42,34 +41,49 @@ uint_least32_t modop( uint_least32_t a, uint_least32_t b)
 }
 
 
-uint_least32_t gcd( uint_least32_t a, uint_least32_t b)
+uint_least32_t gcd( int_least32_t a, int_least32_t b)
 {
 	uint_least32_t r;
-
-	if( (a == 0u)
-			||(b == 0u) )
+	uint_least32_t op1 = (uint_least32_t)a;
+	uint_least32_t op2 = (uint_least32_t)b;
+	if(a < 0)
 	{
-		b+=a;
+		/* Multiplication by -1 or unary use of the - sign breaks MISRA-C:2004 12.9/R so bitwise conversion is used*/
+		op1 = ~op1 + 1u;
+	}
+	if(b < 0)
+	{
+		/* Multiplication by -1 or unary use of the - sign breaks MISRA-C:2004 12.9/R  rule so bitwise conversion is used*/
+		op2 = ~op2 + 1u;
+	}
+	if( (op1 == 0u)
+			||(op2 == 0u) )
+	{
+		op2+=op1;
 	}else{
-		r = modop(a,b);
+		r = modop(op1,op2);
 		while( r != 0u )
 		{
-			a = b;
-			b = r;
-			r = modop(a,b);
+			op1 = op2;
+			op2 = r;
+			r = modop(op1,op2);
 		}
 	}
-	return b;
+	return op2;
 }
 
-uint_least32_t calculate_gcd( const uint_least32_t arr[], const int_least16_t n )
+uint_least32_t calculate_gcd( const int_least32_t arr[], const int_least16_t n )
 {
 	uint_least32_t res = arr[ 0 ];
-	int_least16_t  i;
+	int_least16_t i;
+	if ( arr[ 0 ] < 0 )
+	{
+		res = ~res + 1u;
+	}
 
 	for( i = 1; i < n; i++ )
 	{
-		res = gcd( arr[ i ], res);
+		res = gcd( arr[ i ], (int_least32_t)res);
 	}
 	return res;
 }
